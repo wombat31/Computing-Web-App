43fe8feac7cd376f89b4f6ce6749c56cd595dab8
Justin Welham <jwelham@BCAA.AE>
2019-02-17 21:38:04 +0400

    17.2.19

---
+++ js/comp.js
@@ -0,0 +1,9 @@
+function signUp(){
+    document.getElementById("login").style.display="None";
+    document.getElementById("signUp").style.display="block";
+}
+
+function login(){
+    document.getElementById("signUp").style.display="None";
+    document.getElementById("login").style.display="block";
+}
